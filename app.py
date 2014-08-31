bd237c727ae1ba8c84c44ded045cfeb8ce912d6f
amjd <amjad.4@gmail.com>
2014-08-31 21:57:35 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/livedata/<lat>/<lng>')
+	url = 'https://api.uber.com/v1/estimates/time'
+
+	parameters = {
+	    'server_token': 'xMSF2VlxJybkRJY5SeVFw15XJ8wCPzEBoiVoVfOu',
+	    'start_latitude': lat,
+	    'start_longitude': lng
+	}
+	
+	response = requests.get(url, params=parameters)
+		
+	return json.dumps(response.json(),sort_keys=True, indent=4)
